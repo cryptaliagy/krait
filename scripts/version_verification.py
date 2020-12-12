657de107f7bbbf0e118a937d2387b9b2de976986
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-12-12 10:41:10 -0500

    fix: corrected release issues for windows

--- scripts/version_verification.py
+++ scripts/version_verification.py
@@ -7,6 +7,7 @@ def verify_version_increased(last_version, current_version):
     if not (last_version < current_version):
         print('New version needs to be higher than old version')
         sys.exit(1)
+    print('yes')
     sys.exit(0)
 
 
@@ -16,7 +17,15 @@ def verify_version_equals(last_version, current_version):
     sys.exit(0)
 
 
+def verify_version_nequals(last_version, current_version):
+    if not (last_version == current_version):
+        sys.exit(1)
+    sys.exit(0)
+
+
 if __name__ == '__main__':
     if sys.argv[1] == '--equals':
+        verify_version_equals(Version(sys.argv[2]), Version(sys.argv[3]))
+    if sys.argv[1] == '--nequals':
         verify_version_equals(Version(sys.argv[2]), Version(sys.argv[3]))
     verify_version_increased(Version(sys.argv[1]), Version(sys.argv[2]))
