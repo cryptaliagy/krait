26b8635e5785c0c4ec8441b46948595c9ae8dbea
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-12-17 13:57:19 -0500

    new: integrated .gitignore rendering, resolves #2

--- src/krait/lib/plugins/vcs.py
+++ src/krait/lib/plugins/vcs.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import krait.lib.abc as abc
+import krait.lib.files as kf
+
 from git import Repo  # type: ignore
 from pathlib import Path
+from typing import Any
+
+from krait.utils.templates import env
 
 
 class BaseVCSPlugin(abc.AbstractVCS):
     name: str
+    ignore_file: kf.File
 
     def __init__(
         self,
@@ -17,7 +23,14 @@ class BaseVCSPlugin(abc.AbstractVCS):
         self.name = ''
 
     def initialize(self, project_path: Path):
-        self.file_renderer.output(f'Initializing {self.name} repository...')
+        self.dir_renderer.output(f'Initializing {self.name} repository...')
+
+    def render_ignorefile(self, ignore_file_name: str, **render_params: Any):
+        self.file_renderer.output(f'Adding {ignore_file_name}...')
+        self.ignore_file = kf.File(ignore_file_name)
+        contents = env.get_template(f'{ignore_file_name}.jinja2').render(**render_params)
+        self.ignore_file.add_content(contents)
+        self.file_renderer.add_file(self.ignore_file)
 
 
 class GitPlugin(BaseVCSPlugin):
@@ -35,6 +48,7 @@ class GitPlugin(BaseVCSPlugin):
     def initialize(self, project_path: Path):
         super().initialize(project_path)
         self.repository = Repo.init(project_path.resolve(), mkdir=False)
+        self.render_ignorefile('.gitignore')
 
 
 class NoVCS(BaseVCSPlugin):
