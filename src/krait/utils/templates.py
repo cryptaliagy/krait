5dfb5dbcde582dce8a11b01d0f1ae21c31267b3c
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-12-30 12:02:18 -0500

    fix: converted path to string in template loader

--- src/krait/utils/templates.py
+++ src/krait/utils/templates.py
@@ -3,4 +3,4 @@ import jinja2
 import krait.utils.config as kconfig
 
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(kconfig.get_config_folder() / 'templates'), autoescape=False)  # noqa
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(str(kconfig.get_config_folder() / 'templates')), autoescape=False)  # noqa
